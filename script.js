b387836954f2b69bbe2cecbc3fc6adabd74b1179
Pedro Martins <pedrohenrique.251@hotmail.com>
2023-12-07 17:45:34 -0300

    Correção/teste de git push.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var nome = prompt("Informe o seu primeiro nome:")
 var idade = prompt("Digite a sua idade:")
 
-alert(nome +" " + "tem" + " " + idade + " " + "anos.")
+alert(nome + " " + "tem" + " " + idade + " " + "anos.")
